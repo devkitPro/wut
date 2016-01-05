47f4819213483e78ef6ee756c9555b18fa771676
James Benton <james.benton2@gmail.com>
2016-01-05 15:23:57 -0800

    Fix formatting.

--- rpl/common/lib.c
+++ rpl/common/lib.c
@@ -1,23 +1,23 @@
 #include "config.h"
 
 // Get the start of the stubs
-static const void* fstubBegin[0] __attribute__((section(".data.rplFuncStubs")));
+static const void *fstubBegin[0] __attribute__((section(".data.rplFuncStubs")));
 
 #define EXPORT(name) \
-	extern void* name; \
-	const void* name##_stub __attribute__((section(".data.rplFuncStubs"))) = &name; \
+	extern void *name; \
+	const void *name##_stub __attribute__((section(".data.rplFuncStubs"))) = &name; \
 
 #include "exports.h"
 
 // Get the end of the stubs
-static const void* fstubEnd[0] __attribute__((section(".data.rplFuncStubs")));
+static const void *fstubEnd[0] __attribute__((section(".data.rplFuncStubs")));
 
 // Create a header with the name and stub begin/end in
 typedef struct
 {
-	const char* name;
-	const void* fstubBegin;
-	const void* fstubEnd;
+	const char *name;
+	const void *fstubBegin;
+	const void *fstubEnd;
 } __attribute__((__packed__)) rpl_header;
 
 static const const char name[] __attribute__((section(".rodata.rplNames"))) = LIBRARY_NAME;
