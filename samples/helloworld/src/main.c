f3241113f503c2618d7917b151ecc0a0a3466e31
James Benton <james.benton2@gmail.com>
2016-01-07 04:48:45 -0800

    Update helloworld to say hello from every core.

--- samples/helloworld/src/main.c
+++ samples/helloworld/src/main.c
@@ -1,8 +1,43 @@
+#include <coreinit/core.h>
 #include <coreinit/debug.h>
+#include <coreinit/thread.h>
 
-int main(int argc, char **argv)
+int
+CoreEntryPoint(int argc, const char **argv)
 {
-	OSReport("Testing var args %d", argc);
-	OSFatal("my first rpx");
-	return 0;
+   OSReport("Hello world from %s", argv[0]);
+   return argc;
+}
+
+int
+main(int argc, char **argv)
+{
+   OSReport("Main thread running on core %d", OSGetCoreId());
+
+   // Run thread on core 0
+   OSThread *threadCore0 = OSGetDefaultThread(0);
+
+   const char *core0Args[] = {
+      "Core 0"
+   };
+
+   OSRunThread(threadCore0, CoreEntryPoint, 0, core0Args);
+
+   // Run thread on core 2
+   OSThread *threadCore2 = OSGetDefaultThread(2);
+
+   const char *core2Args[] = {
+      "Core 2"
+   };
+
+   OSRunThread(threadCore2, CoreEntryPoint, 2, core2Args);
+
+   // Wait for threads to return
+   int resultCore0 = -1, resultCore2 = -1;
+   OSJoinThread(threadCore0, &resultCore0);
+   OSJoinThread(threadCore2, &resultCore2);
+
+   OSReport("Core 0 thread returned %d", resultCore0);
+   OSReport("Core 2 thread returned %d", resultCore2);
+   return 0;
 }
