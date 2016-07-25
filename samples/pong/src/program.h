bd39d9e294701e9baf4755ec4f2f7901a323bcce
shinyquagsire23 <mtinc2@gmail.com>
2016-07-25 14:20:15 -0700

    Add pong sample to samples/

---
+++ samples/pong/src/program.h
@@ -0,0 +1,11 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+//Using modified version of draw to render at twice the scale to improve framerate
+#include "draw.h"
+
+#include "pong.h"
+
+void _entryPoint();
+
+#endif /* PROGRAM_H */
