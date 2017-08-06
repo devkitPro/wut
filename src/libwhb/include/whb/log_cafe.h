90d13e57085942b7b193088f18193e9228b0173e
CreeperMario <creepermario2@gmail.com>
2017-08-06 17:42:43 +0930

    libwhb: Add a simplistic Cafe Log handler

---
+++ src/libwhb/include/whb/log_cafe.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <wut.h>
+
+/**
+ * \defgroup whb_log_cafe Cafe OS System Log Output
+ * \ingroup whb
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BOOL
+WHBLogCafeInit();
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
