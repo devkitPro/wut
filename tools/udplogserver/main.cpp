ac4ce95f8477fc5cc55bcb50d57a2d80ccebeca6
CreeperMario <creepermario2@gmail.com>
2017-08-20 17:51:45 +0930

    udplogserver: Flush output buffer when sent a message
    * Because we are no longer adding a new line, which automatically
    flushes the buffer.

--- tools/udplogserver/main.cpp
+++ tools/udplogserver/main.cpp
@@ -91,6 +91,7 @@ int main(int argc, char **argv)
          if (recvd > 0) {
             buffer[recvd] = 0;
             std::cout << buffer;
+            std::cout.flush();
          }
       }
    }
