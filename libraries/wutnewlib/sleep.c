a5a1bac28c425a39a1b370020f14d3430dbb18ea
James Benton <james.benton2@gmail.com>
2018-05-25 17:13:54 +0100

    wutnewlib: Add sleep and usleep implementation.

---
+++ libraries/wutnewlib/sleep.c
@@ -0,0 +1,14 @@
+#include <coreinit/thread.h>
+#include <coreinit/systeminfo.h>
+#include <coreinit/time.h>
+#include <unistd.h>
+
+unsigned sleep(unsigned seconds) {
+   OSSleepTicks(OSSeconds(seconds));
+   return 0;
+}
+
+int usleep(useconds_t microseconds) {
+   OSSleepTicks(OSMicroseconds(microseconds));
+   return 0;
+}
