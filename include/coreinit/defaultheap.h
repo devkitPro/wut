766e832363207702b3d840a23fc100cebaebc340
James Benton <james.benton2@gmail.com>
2018-05-29 10:50:49 +0100

    coreinit: Add default heap data exports.

---
+++ include/coreinit/defaultheap.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <wut.h>
+
+/**
+ * \defgroup coreinit_defaultheap Default Heap
+ * \ingroup coreinit
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void *(*MEMAllocFromDefaultHeapFn)(uint32_t size);
+typedef void *(*MEMAllocFromDefaultHeapExFn)(uint32_t size, int32_t alignment);
+typedef void (*MEMFreeToDefaultHeapFn)(void *ptr);
+
+extern MEMAllocFromDefaultHeapFn MEMAllocFromDefaultHeap;
+extern MEMAllocFromDefaultHeapExFn MEMAllocFromDefaultHeapEx;
+extern MEMFreeToDefaultHeapFn MEMFreeToDefaultHeap;
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
