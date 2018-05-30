86ea2f146640b49bafdfbcfd5b04b195f14434e7
James Benton <james.benton2@gmail.com>
2018-05-30 21:56:29 +0100

    Add a simple .rpl file example.

---
+++ samples/my_first_rpl/my_first_rpl.c
@@ -0,0 +1,20 @@
+#include <coreinit/dynload.h>
+
+int
+my_first_export()
+{
+   return 1;
+}
+
+int
+rpl_main(OSDynLoad_Module module,
+         OSDynLoad_EntryReason reason)
+{
+   if (reason == OS_DYNLOAD_LOADED) {
+      // Do stuff on load
+   } else if (reason == OS_DYNLOAD_UNLOADED) {
+      // Do stuff on unload
+   }
+
+   return 0;
+}
