61a84c50b605cb07d772984949b9a396cc812f84
James Benton <james.benton2@gmail.com>
2018-05-30 18:54:41 +0100

    tools: Add rplexportgen to produce SHT_RPL_EXPORTS.

--- share/wut.cmake
+++ share/wut.cmake
@@ -21,22 +21,29 @@ macro(wut_enable_stdcpp target)
       LINK_FLAGS " -Wl,--whole-archive -lwutstdc++ -Wl,--no-whole-archive")
 endmacro(wut_enable_stdcpp)
 
+macro(wut_add_exports target exports_file)
+   set(RPL_EXPORTS_FILE ${exports_file})
+   if(NOT IS_ABSOLUTE ${exports_file})
+      set(RPL_EXPORTS_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${exports_file}")
+   endif()
+
+   add_custom_command(
+      OUTPUT rpl_exports.s
+      COMMAND ${WUT_RPLEXPORTGEN} ${RPL_EXPORTS_FILE} rpl_exports.s
+      DEPENDS ${RPL_EXPORTS_FILE})
+   target_sources(${target} PRIVATE rpl_exports.s)
+
+   set_source_files_properties(rpl_exports.s PROPERTIES LANGUAGE C)
+endmacro(wut_add_exports)
+
 function(wut_create_rpl target source)
    set(RPL_OPTIONS IS_RPX)
-   set(RPL_SINGLE_ARGS EXPORTS_FILE)
+   set(RPL_SINGLE_ARGS "")
    set(RPL_MULTI_ARGS "")
    cmake_parse_arguments(RPL "${RPL_OPTIONS}" "${RPL_SINGLE_ARGS}" "${RPL_MULTI_ARGS}" "${ARGN}")
 
    if(NOT RPL_IS_RPX)
       set(ELF2RPL_FLAGS ${ELF2RPL_FLAGS} --rpl)
-   endif()
-
-   if(RPL_EXPORTS_FILE)
-      if(NOT IS_ABSOLUTE ${RPL_EXPORTS_FILE})
-         set(RPL_EXPORTS_FILE "${CMAKE_CURRENT_SOURCE_DIR}/${RPL_EXPORTS_FILE}")
-      endif()
-
-      set(ELF2RPL_FLAGS ${ELF2RPL_FLAGS} --exports ${RPL_EXPORTS_FILE})
    endif()
 
    target_link_libraries(${source}
