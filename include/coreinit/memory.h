42ac732b2e9e2a24ee596a803a053b0761dcc32a
James Benton <james.benton2@gmail.com>
2018-05-30 13:11:57 +0100

    coreinit: Add some memory functions.
    Adds OSGetForegroundBucket, OSGetForegroundBucketFreeArea, OSGetMemBound.

--- include/coreinit/memory.h
+++ include/coreinit/memory.h
@@ -12,6 +12,12 @@
 extern "C" {
 #endif
 
+typedef enum OSMemoryType
+{
+   OS_MEM1 = 1,
+   OS_MEM2 = 2,
+} OSMemoryType;
+
 void *
 OSBlockMove(void *dst,
             const void *src,
@@ -23,12 +29,25 @@ OSBlockSet(void *dst,
            uint8_t val,
            uint32_t size);
 
-void*
+void *
 OSAllocFromSystem(uint32_t size,
                   int align);
 
 void
 OSFreeToSystem(void *ptr);
+
+BOOL
+OSGetForegroundBucket(uint32_t *outAddr,
+                      uint32_t *outSize);
+
+BOOL
+OSGetForegroundBucketFreeArea(uint32_t *outAddr,
+                              uint32_t *outSize);
+
+int
+OSGetMemBound(OSMemoryType type,
+              uint32_t *outAddr,
+              uint32_t *outSize);
 
 #ifdef __cplusplus
 }
