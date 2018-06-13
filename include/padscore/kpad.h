9beff5dd0beaf5eda614f51506956156a6ac85b2
James Benton <james.benton2@gmail.com>
2018-06-13 12:11:00 +0100

    Add padscore/kpad.h for a stubbed KPADStatus typedef.

---
+++ include/padscore/kpad.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <wut.h>
+
+/**
+ * \defgroup padscore_kpad KPAD
+ * \ingroup padscore
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct KPADStatus KPADStatus;
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
