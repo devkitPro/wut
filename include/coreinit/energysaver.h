9cc6aead841af2063212d5473fc7361ff96acc57
James Benton <james.benton2@gmail.com>
2018-06-17 21:02:35 +0100

    coreinit: Add remaining IM* functions.

--- include/coreinit/energysaver.h
+++ include/coreinit/energysaver.h
@@ -11,26 +11,52 @@
 extern "C" {
 #endif
 
-int32_t
-IMEnableDim(void);
+typedef int32_t IMError;
 
-int32_t
-IMDisableDim(void);
+IMError
+IMDisableAPD();
 
-int32_t
-IMIsDimEnabled(int32_t * result);
+IMError
+IMDisableDim();
 
-int32_t
-IMEnableAPD(void);
+IMError
+IMEnableAPD();
 
-int32_t
-IMDisableAPD(void);
+IMError
+IMEnableDim();
 
-int32_t
-IMIsAPDEnabled(int32_t * result);
+IMError
+IMIsAPDEnabled(uint32_t *outValue);
 
-int32_t
-IMIsAPDEnabledBySysSettings(int32_t * result);
+IMError
+IMIsAPDEnabledBySysSettings(uint32_t *outValue);
+
+IMError
+IMIsDimEnabled(uint32_t *outValue);
+
+IMError
+IMGetDimEnableDrc(uint32_t *outValue);
+
+IMError
+IMGetDimEnableTv(uint32_t *outValue);
+
+IMError
+IMGetDimPeriod(uint32_t *outValue);
+
+IMError
+IMGetTimeBeforeAPD(uint32_t *outSeconds);
+
+IMError
+IMGetTimeBeforeDimming(uint32_t *outSeconds);
+
+IMError
+IMSetDimEnableDrc(BOOL value);
+
+IMError
+IMSetDimEnableTv(BOOL value);
+
+IMError
+IMStartAPDVideoMode();
 
 #ifdef __cplusplus
 }
