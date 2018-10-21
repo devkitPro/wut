f281a995b3497e96951f88813f8d3d1e5a64cf1f
James Benton <james.benton2@gmail.com>
2018-10-21 15:24:48 +0100

    gx2: Add GX2_COMP_MAP and GX2_SQ_SEL_{RGBA} to utils.h

--- include/gx2/utils.h
+++ include/gx2/utils.h
@@ -17,12 +17,19 @@ typedef enum GX2_SQ_SEL
    GX2_SQ_SEL_Y       = 1,
    GX2_SQ_SEL_Z       = 2,
    GX2_SQ_SEL_W       = 3,
+
+   GX2_SQ_SEL_R       = 0,
+   GX2_SQ_SEL_G       = 1,
+   GX2_SQ_SEL_B       = 2,
+   GX2_SQ_SEL_A       = 3,
+
    GX2_SQ_SEL_0       = 4,
    GX2_SQ_SEL_1       = 5,
    GX2_SQ_SEL_MASK    = 7,
 } GX2_SQ_SEL;
 
 #define GX2_SEL_MASK(x, y, z, w) (((x) << 24) | ((y) << 16) | ((z) << 8) | (w))
+#define GX2_COMP_MAP(x, y, z, w) (((x) << 24) | ((y) << 16) | ((z) << 8) | (w))
 
 #ifdef __cplusplus
 }
