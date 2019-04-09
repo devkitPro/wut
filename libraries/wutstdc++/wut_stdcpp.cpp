869fe709ea0f2ddee28cc9f8a9e3e1dd9bfb37b2
Ash Logan <ash@heyquark.com>
2019-04-09 20:26:35 +1000

    Revert "wutstdc++: Move __gxx_personality to dedicated Assembly file"
    This reverts commit 370eeb28e94ccfcff981842bc55a16258fec252c.
    I didn't know what this did, sorry.

--- libraries/wutstdc++/wut_stdcpp.cpp
+++ libraries/wutstdc++/wut_stdcpp.cpp
@@ -1,5 +1,16 @@
 #include "wut_gthread.h"
 
+__asm__ __volatile__(
+   "\t.section\t.text.__wrap___gxx_personality_v0,\"ax\",@progbits\n"
+   "\t.align\t2\n"
+   "\t.globl\t__wrap___gxx_personality_v0\n"
+   "\t.type\t__wrap___gxx_personality_v0, @function\n"
+   "__wrap___gxx_personality_v0:\n"
+   "\t.cfi_startproc\n"
+   "\tb\t__real___gxx_personality_v0\n"
+   "\t.cfi_endproc"
+);
+
 extern "C" void
 __init_wut_stdcpp()
 {
