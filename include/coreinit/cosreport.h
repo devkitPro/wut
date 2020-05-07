d78a564347db669bcf26b553374ab619345c5fdf
Maschell <Maschell@gmx.de>
2020-05-07 10:36:02 +0200

    Add coreinit/cosreport.h - add enum COSReportLevel - add enum COSReportModule - add function COSVReport - add function COSError - add function COSInfo - add function COSVerbose - add function COSWarn
    see decaf for reference:
    - https://github.com/decaf-emu/decaf-emu/blob/dcd9bd1a084ac2806676ea1197c500edfe987f5a/src/libdecaf/src/cafe/libraries/coreinit/coreinit_cosreport.h
    - https://github.com/decaf-emu/decaf-emu/blob/1dc2c3a27832c4c0bcfb6dcac3ce0e64532ddf62/src/libdecaf/src/cafe/libraries/coreinit/coreinit_enum.h

---
+++ include/coreinit/cosreport.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <wut.h>
+
+/**
+ * \defgroup coreinit_cosreport COS Report
+ * \ingroup coreinit
+ *
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct enum COSReportLevel{
+   COS_REPORT_LEVEL_ERROR	= 	0,
+   COS_REPORT_LEVEL_WARN	= 	1,
+   COS_REPORT_LEVEL_INFO	= 	2,
+   COS_REPORT_LEVEL_VERBOSE	= 	3
+} COSReportLevel;
+
+struct enum COSReportModule{
+   COS_REPORT_MODULE_UNKNOWN_0	= 	0,
+   COS_REPORT_MODULE_UNKNOWN_1	= 	1,
+   COS_REPORT_MODULE_UNKNOWN_2	= 	2,
+   COS_REPORT_MODULE_UNKNOWN_5	= 	5
+} COSReportModule;
+
+void
+COSVReport(COSReportModule module,
+           COSReportLevel level,
+           const char* fmt,
+           ...);
+
+void
+COSError(COSReportModule module,
+         const char* fmt,
+         ...);
+
+void
+COSInfo(COSReportModule module,
+        const char* fmt,
+        ...);
+
+void
+COSVerbose(COSReportModule module,
+           const char* fmt,
+           ...);
+
+void
+COSWarn(COSReportModule module,
+        const char* fmt,
+        ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
