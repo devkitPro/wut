de1b4c3cdabcc89da29f8993c91d3ac70fadb8b1
rw-r-r-0644 <r.r.qwertyuiop.r.r@gmail.com>
2020-05-10 00:11:11 +0200

    nsysnet/socket.h: Add retrocompatibility layer for the new socket library

---
+++ include/nsysnet/socket.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <wut.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#warning "nsysnet/socket.h is deprecated; please use standard socket headers instead."
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NSN_EAGAIN          EAGAIN
+#define NSN_EWOULDBLOCK     EWOULDBLOCK
+
+__attribute__ ((deprecated))
+static inline void
+socket_lib_init()
+{
+}
+
+__attribute__ ((deprecated))
+static inline void
+socket_lib_finish()
+{
+}
+
+__attribute__ ((deprecated))
+static inline int
+socketclose(int sockfd)
+{
+   return close(sockfd);
+}
+
+__attribute__ ((deprecated))
+static inline int
+socketlasterr()
+{
+   return errno;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
