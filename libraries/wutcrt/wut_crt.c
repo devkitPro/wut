f9acad4c7cefcb4b708e4caf91e74c3ca86f1138
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2021-06-06 22:47:37 +0200

    Add missing __libc_init_array and __libc_fini_array calls

--- libraries/wutcrt/wut_crt.c
+++ libraries/wutcrt/wut_crt.c
@@ -3,12 +3,17 @@ void __init_wut_newlib();
 void __init_wut_stdcpp();
 void __init_wut_devoptab();
 void __attribute__((weak)) __init_wut_socket();
+void __libc_init_array(void);
 
 void __fini_wut_malloc();
 void __fini_wut_newlib();
 void __fini_wut_stdcpp();
 void __fini_wut_devoptab();
 void __attribute__((weak)) __fini_wut_socket();
+void __libc_fini_array(void);
+
+void
+_init(void) { }
 
 void __attribute__((weak))
 __init_wut()
@@ -18,11 +23,16 @@ __init_wut()
    __init_wut_stdcpp();
    __init_wut_devoptab();
    if (&__init_wut_socket) __init_wut_socket();
+   __libc_init_array();
 }
+
+void
+_fini(void) { }
 
 void __attribute__((weak))
 __fini_wut()
 {
+   __libc_fini_array();
    if (&__fini_wut_socket) __fini_wut_socket();
    __fini_wut_devoptab();
    __fini_wut_stdcpp();
