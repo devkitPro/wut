de9c8be777175322d641f68ae1bf45fc1450df28
LRFLEW <LRFLEW@aol.com>
2021-11-03 16:33:01 -0500

    Add OSForceFullRelaunch() declaration

---
+++ include/coreinit/launch.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <wut.h>
+
+/**
+ * \defgroup coreinit_launch Launch
+ * \ingroup coreinit
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void
+OSForceFullRelaunch();
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
