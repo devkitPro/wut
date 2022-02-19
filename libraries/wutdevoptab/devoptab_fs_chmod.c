d95ebc363b7eb7c94eec09acd6de6a6c53a42d0d
Maschell <Maschell@gmx.de>
2022-02-19 14:44:50 +0100

    wutdevoptab: Fix chmod mode translation

--- libraries/wutdevoptab/devoptab_fs_chmod.c
+++ libraries/wutdevoptab/devoptab_fs_chmod.c
@@ -19,6 +19,37 @@ __wut_fs_chmod(struct _reent *r,
       return -1;
    }
 
+   FSMode translatedMode = 0;
+   if (mode & S_IRUSR) {
+      translatedMode |= FS_MODE_READ_OWNER;
+   }
+   if (mode & S_IWUSR) {
+      translatedMode |= FS_MODE_WRITE_OWNER;
+   }
+   if (mode & S_IXUSR) {
+      translatedMode |= FS_MODE_EXEC_OWNER;
+   }
+
+   if (mode & S_IRGRP) {
+      translatedMode |= FS_MODE_READ_GROUP;
+   }
+   if (mode & S_IWGRP) {
+      translatedMode |= FS_MODE_WRITE_GROUP;
+   }
+   if (mode & S_IXGRP) {
+      translatedMode |= FS_MODE_EXEC_GROUP;
+   }
+
+   if (mode & S_IROTH) {
+      translatedMode |= FS_MODE_READ_OTHER;
+   }
+   if (mode & S_IWOTH) {
+      translatedMode |= FS_MODE_WRITE_OTHER;
+   }
+   if (mode & S_IXOTH) {
+      translatedMode |= FS_MODE_EXEC_OTHER;
+   }
+
    FSInitCmdBlock(&cmd);
    status = FSChangeMode(__wut_devoptab_fs_client, &cmd, fixedPath,
                          translatedMode, 0x777, FS_ERROR_FLAG_ALL);
