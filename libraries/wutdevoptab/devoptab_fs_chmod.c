6bfff87662fcb2881b8f3333e51660dcd01d2856
Maschell <Maschell@gmx.de>
2022-02-26 16:55:47 +0100

    coreinit: Add missing parameter to FSChangeMode/FSChangeModeAsync (#199)

--- libraries/wutdevoptab/devoptab_fs_chmod.c
+++ libraries/wutdevoptab/devoptab_fs_chmod.c
@@ -1,4 +1,12 @@
+#include <sys/stat.h>
 #include "devoptab_fs.h"
+
+static inline FSMode
+__wut_fs_convert_mode(mode_t mode)
+{
+   // Convert normal Unix octal permission bits into CafeOS hexadecimal permission bits
+   return (FSMode) (((mode & S_IRWXU) << 2) | ((mode & S_IRWXG) << 1) | (mode & S_IRWXO));
+}
 
 int
 __wut_fs_chmod(struct _reent *r,
@@ -18,9 +26,11 @@ __wut_fs_chmod(struct _reent *r,
       return -1;
    }
 
+   FSMode translatedMode = __wut_fs_convert_mode(mode);
+
    FSInitCmdBlock(&cmd);
    status = FSChangeMode(__wut_devoptab_fs_client, &cmd, fixedPath,
-                         (FSMode)mode, FS_ERROR_FLAG_ALL);
+                         translatedMode, 0x777, FS_ERROR_FLAG_ALL);
    free(fixedPath);
    if (status < 0) {
       r->_errno = __wut_fs_translate_error(status);
