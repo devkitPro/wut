03c3d42d38de8139b5f883622c20bcda7f23f3e0
fincs <fincs@devkitpro.org>
2022-03-05 17:57:54 +0100

    wutnewlib: replace dynamic allocation of OSMutex with static table

--- libraries/wutnewlib/wut_lock.c
+++ libraries/wutnewlib/wut_lock.c
@@ -3,57 +3,78 @@
 #include <coreinit/mutex.h>
 #include <malloc.h>
 
+#define MAX_LOCKS 16
+
+static OSMutex sLibcLocks[MAX_LOCKS];
+static uint32_t sLibcLockUsedMask = 0;
+
+static inline bool
+__wut_is_lock_valid(int *lock)
+{
+   return lock && *lock >= 0 && *lock < MAX_LOCKS;
+}
+
 int
 __wut_lock_init(int *lock,
                 int recursive)
 {
-   OSMutex *mutex = NULL;
    if (!lock) {
       return -1;
    }
 
-   mutex = (OSMutex *)malloc(sizeof(OSMutex));
-   if (!mutex) {
+   int slot;
+   uint32_t new_mask;
+   uint32_t cur_mask = __atomic_load_n(&sLibcLockUsedMask, __ATOMIC_SEQ_CST);
+   do {
+      slot = __builtin_ffs(~cur_mask)-1;
+      if (slot < 0 || slot >= MAX_LOCKS) break;
+      new_mask = cur_mask | (1U << slot);
+   } while (!__atomic_compare_exchange_n(&sLibcLockUsedMask, &cur_mask, new_mask, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST));
+
+   if (slot < 0 || slot >= MAX_LOCKS) {
       return -1;
    }
 
-   OSInitMutex(mutex);
-   *lock = (int)mutex;
+   *lock = slot;
+   OSInitMutex(&sLibcLocks[*lock]);
    return 0;
 }
 
 int
 __wut_lock_close(int *lock)
 {
-   if (!lock || *lock == 0) {
+   if (!__wut_is_lock_valid(lock)) {
       return -1;
    }
 
-   free((void *)*lock);
-   *lock = 0;
+   uint32_t new_mask;
+   uint32_t cur_mask = __atomic_load_n(&sLibcLockUsedMask, __ATOMIC_SEQ_CST);
+   do {
+      new_mask = cur_mask &~ (1U << *lock);
+   } while (!__atomic_compare_exchange_n(&sLibcLockUsedMask, &cur_mask, new_mask, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST));
+
+   *lock = -1;
    return 0;
 }
 
 int
 __wut_lock_acquire(int *lock)
 {
-   OSMutex *mutex = (OSMutex *)*lock;
-   if (!lock || *lock == 0) {
+   if (!__wut_is_lock_valid(lock)) {
       return -1;
    }
 
-   OSLockMutex(mutex);
+   OSLockMutex(&sLibcLocks[*lock]);
    return 0;
 }
 
 int
 __wut_lock_release(int *lock)
 {
-   OSMutex *mutex = (OSMutex *)*lock;
-   if (!lock || *lock == 0) {
+   if (!__wut_is_lock_valid(lock)) {
       return -1;
    }
 
-   OSUnlockMutex(mutex);
+   OSUnlockMutex(&sLibcLocks[*lock]);
    return 0;
 }
