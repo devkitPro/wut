8ac8dc5ed677457c1a0a13de502450b514b40418
Maschell <Maschell@gmx.de>
2022-05-07 12:26:11 +0200

    wutdevoptab: Add time and mode information for stat and fstat (#218)
    * wutdevoptab: Add time and mode information for stat and fstat
    
    * wutdevoptab: rename __wut_translate_mode to __wut_fs_translate_mode

--- libraries/wutdevoptab/devoptab_fs_stat.c
+++ libraries/wutdevoptab/devoptab_fs_stat.c
@@ -3,11 +3,10 @@
 int
 __wut_fs_stat(struct _reent *r,
               const char *path,
-              struct stat *st)
-{
-   int fd;
+              struct stat *st) {
    FSStatus status;
    FSCmdBlock cmd;
+   FSStat fsStat;
 
    if (!path || !st) {
       r->_errno = EINVAL;
@@ -15,35 +14,34 @@ __wut_fs_stat(struct _reent *r,
    }
 
    char *fixedPath = __wut_fs_fixpath(r, path);
-   if (!fixedPath)  {
+   if (!fixedPath) {
       return -1;
    }
 
    FSInitCmdBlock(&cmd);
 
    // First try open as file
-   status = FSOpenFile(__wut_devoptab_fs_client, &cmd, fixedPath, "r",
-                       (FSFileHandle*)&fd, FS_ERROR_FLAG_ALL);
-   if (status >= 0) {
-      __wut_fs_file_t tmpfd = { .fd = fd };
-      status = __wut_fs_fstat(r, &tmpfd, st);
-      FSCloseFile(__wut_devoptab_fs_client, &cmd, fd, FS_ERROR_FLAG_ALL);
-      free(fixedPath);
-      return status;
-   }
-
-   // File failed, so lets try open as directory
-   status = FSOpenDir(__wut_devoptab_fs_client, &cmd, fixedPath,
-                      (FSDirectoryHandle*)&fd, FS_ERROR_FLAG_ALL);
-   free(fixedPath);
+   status = FSGetStat(__wut_devoptab_fs_client, &cmd, fixedPath, &fsStat, FS_ERROR_FLAG_ALL);
    if (status < 0) {
+      free(fixedPath);
       r->_errno = __wut_fs_translate_error(status);
       return -1;
    }
+   free(fixedPath);
 
    memset(st, 0, sizeof(struct stat));
+
    st->st_nlink = 1;
-   st->st_mode = S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO;
-   FSCloseDir(__wut_devoptab_fs_client, &cmd, fd, FS_ERROR_FLAG_ALL);
+   st->st_atime = __wut_fs_translate_time(fsStat.modified);
+   st->st_ctime = __wut_fs_translate_time(fsStat.created);
+   st->st_mtime = __wut_fs_translate_time(fsStat.modified);
+   st->st_mode = __wut_fs_translate_mode(fsStat);
+
+   if (!(fsStat.flags & FS_STAT_DIRECTORY)) {
+      st->st_size = fsStat.size;
+      st->st_uid = fsStat.owner;
+      st->st_gid = fsStat.group;
+   }
+
    return 0;
 }
