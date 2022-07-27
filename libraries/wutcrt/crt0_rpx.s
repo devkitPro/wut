933989083905b331914aa00220d615269d5f345d
Maschell <Maschell@gmx.de>
2022-07-27 21:08:58 +0200

    wutcrt: Make sure main is called with a 8 byte aligned stack pointer

--- libraries/wutcrt/crt0_rpx.s
+++ libraries/wutcrt/crt0_rpx.s
@@ -7,16 +7,16 @@
 .section .crt0, "ax", @progbits
 .global __rpx_start
 __rpx_start:
-   stwu 1, -0x14(1)
+   stwu 1, -0x10(1)
    mflr 0
-   stw 0, 0x18(1)
-   stw 3, 0xC(1)
-   stw 4, 0x10(1)
+   stw 0, 0x14(1)
+   stw 3, 0x8(1)
+   stw 4, 0xC(1)
    bl __init_wut
-   lwz 3, 0xC(1)
-   lwz 4, 0x10(1)
+   lwz 3, 0x8(1)
+   lwz 4, 0xC(1)
    bl main
-   addi 1, 1, 0x14
+   addi 1, 1, 0x10
    b exit
 
 # -----------------------------------
