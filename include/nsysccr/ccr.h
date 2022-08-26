23e7bd8d026def1b32d13df3d89244edb32ce1ca
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2022-08-26 23:40:01 +0200

    Add several nsysccr functions

---
+++ include/nsysccr/ccr.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <wut.h>
+
+/**
+ * \defgroup nsysccr_ccr
+ * \ingroup nsysccr
+ *
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t
+CCRSetCompatMode(uint32_t compatMode);
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
