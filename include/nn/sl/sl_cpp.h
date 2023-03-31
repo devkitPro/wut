9f863c7821ac4ef253e832cc26496c44ac38ac12
Maschell <Maschell@gmx.de>
2023-03-31 14:50:45 +0200

    nn::sl: Add more fields to LaunchInfo struct

--- include/nn/sl/sl_cpp.h
+++ include/nn/sl/sl_cpp.h
@@ -3,6 +3,7 @@
 #include <wut.h>
 #include <nn/result.h>
 #include <coreinit/memdefaultheap.h>
+#include <coreinit/mcp.h>
 
 #ifdef __cplusplus
 
@@ -10,12 +11,24 @@ namespace nn {
 
 namespace sl {
 
+typedef enum MediaType {
+    NN_SL_MEDIA_TYPE_MLC  = 0x00,
+    NN_SL_MEDIA_TYPE_SLC  = 0x01,
+    NN_SL_MEDIA_TYPE_ODD  = 0x02,
+    NN_SL_MEDIA_TYPE_USB  = 0x03,
+    NN_SL_MEDIA_TYPE_HFIO = 0x04,
+} MediaType;
+
 typedef struct WUT_PACKED LaunchInfo {
     uint64_t titleId;
-    WUT_UNKNOWN_BYTES(0x810 - 0x08);
+    MCPAppType appType;
+    MediaType mediaType;
+    WUT_UNKNOWN_BYTES(0x800);
 } LaunchInfo;
 
 WUT_CHECK_OFFSET(LaunchInfo, 0x00, titleId);
+WUT_CHECK_OFFSET(LaunchInfo, 0x08, appType);
+WUT_CHECK_OFFSET(LaunchInfo, 0x0C, mediaType);
 WUT_CHECK_SIZE(LaunchInfo, 0x810);
 
 typedef enum Region {
