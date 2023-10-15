0dc1d00746c7da975d86caa77f0223bbdb596329
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2023-10-15 12:26:51 +0200

    Add missing setsockopt optnames

--- include/netinet/in.h
+++ include/netinet/in.h
@@ -15,17 +15,31 @@
 
 #define IP_TOS             3
 #define IP_TTL             4
+#define IP_MULTICAST_IF    9
+#define IP_MULTICAST_TTL   10
+#define IP_MULTICAST_LOOP  11
+#define IP_ADD_MEMBERSHIP  12
+#define IP_DROP_MEMBERSHIP 13
+#define IP_UNKNOWN         14
 
 typedef uint16_t in_port_t;
 typedef uint32_t in_addr_t;
 
-struct in_addr {
-	in_addr_t       s_addr;
+struct in_addr
+{
+   in_addr_t       s_addr;
 };
 
-struct sockaddr_in {
-	sa_family_t     sin_family;
-	in_port_t       sin_port;
-	struct in_addr  sin_addr;
-	unsigned char   sin_zero[8];
+struct sockaddr_in
+{
+   sa_family_t     sin_family;
+   in_port_t       sin_port;
+   struct in_addr  sin_addr;
+   unsigned char   sin_zero[8];
+};
+
+struct ip_mreq
+{
+   struct in_addr imr_multiaddr;
+   struct in_addr imr_interface;
 };
--- include/netinet/tcp.h
+++ include/netinet/tcp.h
@@ -6,3 +6,4 @@
 #define TCP_NOACKDELAY     0x2002
 #define TCP_MAXSEG         0x2003
 #define TCP_NODELAY        0x2004
+#define TCP_UNKNOWN        0x2005   // amount of mss received before sending an ack
