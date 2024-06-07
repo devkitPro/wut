0ca0cbb98362ec218b57787949ed14890df0fcec
Maschell <Maschell@gmx.de>
2024-06-07 16:01:45 +0200

    avm: Add some missing DRC functions

--- include/avm/drc.h
+++ include/avm/drc.h
@@ -26,6 +26,30 @@ typedef enum AVMDrcMode
    AVM_DRC_MODE_DOUBLE = 2,
 } AVMDrcMode;
 
+typedef enum AVMDrcSystemAudioMode
+{
+    AVM_DRC_SYSTEM_AUDIO_MODE_UNKNOWN_0 = 0, // mono?
+    AVM_DRC_SYSTEM_AUDIO_MODE_UNKNOWN_1 = 1, // stereo?
+    AVM_DRC_SYSTEM_AUDIO_MODE_SURROUND  = 2,
+} AVMDrcSystemAudioMode;
+
+/**
+ * Uses AVMGetDRCSystemAudioMode internally
+ * @param outAudioMode
+ * @return 0 on success, -2 on error
+ */
+int
+AVMGetSystemDRCAudioMode(AVMDrcSystemAudioMode *outAudioMode);
+
+BOOL
+AVMGetDRCSystemAudioMode(AVMDrcSystemAudioMode *outAudioMode);
+
+uint32_t
+AVMGetDRCVertCount();
+
+BOOL
+AVMIsDRCFirstFlippDone();
+
 BOOL
 AVMGetDRCScanMode(AVMDrcScanMode *outScanMode);
 
@@ -34,6 +58,12 @@ AVMGetDRCMode(AVMDrcMode *outMode);
 
 uint32_t
 AVMProbeDRCNum(void);
+
+BOOL
+AVMSetDRCEnable(BOOL enable);
+
+BOOL
+AVMSetDRCGamma(float *gamma);
 
 #ifdef __cplusplus
 }
