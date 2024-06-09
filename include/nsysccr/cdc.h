bfe6f38c092b1a01be3ab266ac93527cff8be8cb
Maschell <Maschell@gmx.de>
2024-06-09 13:25:06 +0200

    Update include/nsysccr/cdc.h
    Co-authored-by: GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>

--- include/nsysccr/cdc.h
+++ include/nsysccr/cdc.h
@@ -51,7 +51,7 @@ typedef enum CCRCDCDrcStateEnum
    CCR_CDC_DRC_STATE_BACKGROUND          = 4,
    CCR_CDC_DRC_STATE_DISCONNECT          = 5,
    CCR_CDC_DRC_STATE_IN_QUICK_START_MENU = 12,
-   CCR_CDC_DRC_STATE_UNK3_15             = 15,
+   CCR_CDC_DRC_STATE_UNK15               = 15,
 } CCRCDCDrcStateEnum;
 
 typedef enum CCRCDCWakeStateEnum
