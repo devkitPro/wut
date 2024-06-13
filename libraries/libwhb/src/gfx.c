d77935ff4be29fe395c7fd0f442c148f13b6d0bc
jranderia3 <94210600+jranderia3@users.noreply.github.com>
2024-06-13 16:23:21 -0400

    Update gfx.c
    newline fix

--- libraries/libwhb/src/gfx.c
+++ libraries/libwhb/src/gfx.c
@@ -310,6 +310,7 @@ WHBGfxInit()
    sDrcRenderMode = GX2GetSystemDRCMode();
    sTvSurfaceFormat = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
    sDrcSurfaceFormat = GX2_SURFACE_FORMAT_UNORM_R8_G8_B8_A8;
+   
    switch(GX2GetSystemTVScanMode())
    {
    case GX2_TV_SCAN_MODE_480I:
