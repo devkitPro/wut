12a20e648fbbd44690c0eccf6af8c205b59b4ea4
capitalistspz <keipitalists@proton.me>
2024-09-02 21:22:22 +0100

    Correct offset check

--- include/camera/camera.h
+++ include/camera/camera.h
@@ -92,7 +92,7 @@ struct CAMEventData
    };
 };
 WUT_CHECK_OFFSET(CAMEventData, 0x00, eventType);
-WUT_CHECK_OFFSET(CAMEventData, 0x04, decode.surface);
+WUT_CHECK_OFFSET(CAMEventData, 0x04, decode.surfaceBuffer);
 WUT_CHECK_OFFSET(CAMEventData, 0x08, decode.handle);
 WUT_CHECK_OFFSET(CAMEventData, 0x0c, decode.failed);
 WUT_CHECK_OFFSET(CAMEventData, 0x04, detach.connected);
