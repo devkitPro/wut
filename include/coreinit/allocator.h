60e99ced56031b4739e7654db58e5a501c4c451e
Daniel K. O. (dkosmari) <none@none>
2024-10-01 11:55:50 -0300

    Added header for allocator API.
    --HG--
    branch : coreinit-allocator-api

---
+++ include/coreinit/allocator.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <wut.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct MEMAllocatorFunctions MEMAllocatorFunctions;
+
+
+typedef struct MEMAllocator {
+   MEMAllocatorFunctions *funcs;
+   MEMHeapHandle          heap;
+   uint32_t               arg1;
+   uint32_t               arg2;
+} MEMAllocator;
+WUT_CHECK_SIZE(MEMAllocator, 0x10);
+
+
+
+typedef void* (*MEMAllocatorAlloc)(MEMAllocator *allocator, uint32_t size);
+typedef void (*MEMAllocatorFree)(MEMAllocator *allocator, void *ptr);
+
+
+struct MEMAllocatorFunctions {
+   MEMAllocatorAlloc alloc;
+   MEMAllocatorFree  free;
+};
+WUT_CHECK_SIZE(MEMAllocatorFunctions, 0x8);
+
+
+void* MEMAllocFromAllocator(MEMAllocator *allocator, uint32_t size);
+void MEMFreeToAllocator(MEMAllocator *allocator, void *ptr);
+
+
+void MEMInitAllocatorForExpHeap(MEMAllocator *allocator,
+                                MEMHeapHandle heap,
+                                uint32_t alignment);
+
+
+void MEMInitAllocatorForFrmHeap(MEMAllocator *allocator,
+                                MEMHeapHandle heap,
+                                uint32_t alignment);
+
+
+void MEMInitAllocatorForUnitHeap(MEMAllocator *allocator, MEMHeapHandle heap);
+
+
+void MEMInitAllocatorForDefaultHeap(MEMAllocator *allocator);
+
+
+void MEMInitAllocatorForBlockHeap(MEMAllocator *allocator,
+                                  MEMHeapHandle heap,
+                                  uint32_t alignment);
+
+#ifdef __cplusplus
+}
+#endif
