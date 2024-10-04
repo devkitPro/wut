3a0507a0db54024aeb13de2f59e09418e847b09d
Daniel K. O. (dkosmari) <none@none>
2024-10-04 09:07:33 -0300

    Added Doxygen docs.
    --HG--
    branch : coreinit-allocator-api

--- include/coreinit/allocator.h
+++ include/coreinit/allocator.h
@@ -2,6 +2,16 @@
 #include <wut.h>
 #include "memheap.h"
 
+/**
+ * \defgroup coreinit_allocator Allocator
+ * \ingroup coreinit
+ *
+ * Functions for managing generic allocator objects.
+ *
+ * A `MEMAllocator` object holds pointers to functions to allocate and free memory, as
+ * well as context information to be used by these functions.
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -9,14 +19,16 @@ extern "C" {
 typedef struct MEMAllocatorFunctions MEMAllocatorFunctions;
 typedef struct MEMAllocator MEMAllocator;
 
-typedef void * (*MEMAllocatorAllocFn)(MEMAllocator *allocator,
-                                      uint32_t size);
-typedef void   (*MEMAllocatorFreeFn) (MEMAllocator *allocator,
-                                      void *ptr);
+typedef void * (*MEMAllocatorAllocFn)(MEMAllocator *allocator, uint32_t size);
+typedef void   (*MEMAllocatorFreeFn) (MEMAllocator *allocator, void *ptr);
 
+//! The allocator instance.
 struct MEMAllocator {
+   //! Points to the allocator implementation.
    MEMAllocatorFunctions *funcs;
+   //! The heap handle.
    MEMHeapHandle          heap;
+   //! The alignment the allocator will use.
    uint32_t               align;
    WUT_UNKNOWN_BYTES(4);
 };
@@ -25,6 +37,7 @@ WUT_CHECK_OFFSET(MEMAllocator, 0x4, heap);
 WUT_CHECK_OFFSET(MEMAllocator, 0x8, align);
 WUT_CHECK_SIZE(MEMAllocator, 0x10);
 
+//! The allocator implementation.
 struct MEMAllocatorFunctions {
    MEMAllocatorAllocFn alloc;
    MEMAllocatorFreeFn  free;
@@ -33,31 +46,56 @@ WUT_CHECK_OFFSET(MEMAllocatorFunctions, 0x0, alloc);
 WUT_CHECK_OFFSET(MEMAllocatorFunctions, 0x4, free);
 WUT_CHECK_SIZE(MEMAllocatorFunctions, 0x8);
 
+/**
+ * Allocates memory from the allocator.
+ *
+ * \return `allocator->funcs.alloc(allocator, size)`.
+ */
 void *
 MEMAllocFromAllocator(MEMAllocator *allocator,
                       uint32_t size);
 
+/**
+ * Frees memory back to the allocator.
+ *
+ * It simply calls `allocator->funcs.free(allocator, ptr)`.
+ */
 void
 MEMFreeToAllocator(MEMAllocator *allocator,
                    void *ptr);
 
+/**
+ * Initializes an allocator from an Expanded Heap.
+ */
 void
 MEMInitAllocatorForExpHeap(MEMAllocator *allocator,
                            MEMHeapHandle heap,
                            uint32_t alignment);
 
+/**
+ * Initializes an allocator from a Frame Heap.
+ */
 void
 MEMInitAllocatorForFrmHeap(MEMAllocator *allocator,
                            MEMHeapHandle heap,
                            uint32_t alignment);
 
+/**
+ * Initializes an allocator from a Unit Heap.
+ */
 void
 MEMInitAllocatorForUnitHeap(MEMAllocator *allocator,
                             MEMHeapHandle heap);
 
+/**
+ * Initializes an allocator from the Default Heap.
+ */
 void
 MEMInitAllocatorForDefaultHeap(MEMAllocator *allocator);
 
+/**
+ * Initializes an allocator from a Block Heap.
+ */
 void
 MEMInitAllocatorForBlockHeap(MEMAllocator *allocator,
                              MEMHeapHandle heap,
@@ -66,3 +104,5 @@ MEMInitAllocatorForBlockHeap(MEMAllocator *allocator,
 #ifdef __cplusplus
 }
 #endif
+
+/** @} */
