0fe459a25d64906c918693d32590caee8f307337
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2025-05-11 19:35:59 +0200

    wutsocket/ioctl: Remove unimplemented requests

--- include/sys/ioctl.h
+++ include/sys/ioctl.h
@@ -1,5 +1,17 @@
 #pragma once
 
-#include <sys/ioccom.h>
+#define FIONBIO     (0x8004667E)
+#define FIONREAD    (0x4004667F)
+#define FIONWRITE   (0x40046677)
 
-#include <sys/filio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ioctl(int fd,
+          int request,
+          ...);
+
+#ifdef __cplusplus
+}
+#endif
--- libraries/wutsocket/ioctl.c
+++ libraries/wutsocket/ioctl.c
@@ -6,19 +6,9 @@ ioctl(int fd,
       int request,
       ...)
 {
-   void *data;
    va_list args;
    int sockfd;
    int rc;
-
-   va_start(args, request);
-   data = (request & IOC_INOUT) ? va_arg(args, void *) : NULL;
-   va_end(args);
-
-   if(data == NULL && (request & IOC_INOUT) && IOCPARM_LEN(request) != 0) {
-      errno = EFAULT;
-      return -1;
-   }
 
    sockfd = __wut_get_nsysnet_fd(fd);
    if (sockfd == -1) {
@@ -27,32 +17,43 @@ ioctl(int fd,
 
    switch(request) {
       case FIONBIO: {
+         va_start(args, request);
+         int *data = va_arg(args, int *);
+         va_end(args);
+
+         if(data == NULL) {
+            errno = EFAULT;
+            return -1;
+         }
+
          int flags = fcntl(fd, F_GETFL, 0);
          if(flags == -1) {
             return -1;
          }
-         flags = *(int *)data != 0 ? (flags | O_NONBLOCK) : (flags & ~O_NONBLOCK);
+         flags = (*data != 0) ? (flags | O_NONBLOCK) : (flags & ~O_NONBLOCK);
          return fcntl(fd, F_SETFL, flags);
       }
-      case FIONREAD: {
-         socklen_t optlen = sizeof(int32_t);
-         rc = RPLWRAP(getsockopt)(sockfd,
-                                  SOL_SOCKET,
-                                  SO_RXDATA,
-                                  data,
-                                  &optlen);
-         return __wut_get_nsysnet_result(NULL, rc);
-      }
+      case FIONREAD:
       case FIONWRITE: {
          socklen_t optlen = sizeof(int32_t);
+         va_start(args, request);
+         int32_t *data = va_arg(args, int32_t *);
+         va_end(args);
+
+         if(data == NULL) {
+            errno = EFAULT;
+            return -1;
+         }
+
          rc = RPLWRAP(getsockopt)(sockfd,
                                   SOL_SOCKET,
-                                  SO_TXDATA,
+                                  (request == FIONREAD) ? SO_RXDATA : SO_TXDATA,
                                   data,
                                   &optlen);
          return __wut_get_nsysnet_result(NULL, rc);
       }
       default:
+         errno = ENOTTY;
          break;
    }
 
