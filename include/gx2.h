809d0623f18b149f61c694b303429bd7883c76eb
Daniel K. O. (dkosmari) <none@none>
2026-06-01 17:14:12 -0300

    Prefer top-level headers.

---
+++ include/gx2.h
@@ -0,0 +1,28 @@
+#pragma once
+
+/**
+ * \ingroup gx2
+ * This is a convenience header to include all gx2 headers at once.
+ */
+
+#include "gx2/aperture.h"
+#include "gx2/clear.h"
+#include "gx2/context.h"
+#include "gx2/debug.h"
+#include "gx2/display.h"
+#include "gx2/displaylist.h"
+#include "gx2/draw.h"
+#include "gx2/enum.h"
+#include "gx2/event.h"
+#include "gx2/mem.h"
+#include "gx2/registers.h"
+#include "gx2/sampler.h"
+#include "gx2/semaphore.h"
+#include "gx2/shaders.h"
+#include "gx2/state.h"
+#include "gx2/surface.h"
+#include "gx2/swap.h"
+#include "gx2/temp.h"
+#include "gx2/tessellation.h"
+#include "gx2/texture.h"
+#include "gx2/utils.h"
---
+++ include/gx2r.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * \ingroup gx2r
+ * This is a convenience header to include all gx2r headers at once.
+ */
+
+#include "gx2r/buffer.h"
+#include "gx2r/displaylist.h"
+#include "gx2r/draw.h"
+#include "gx2r/mem.h"
+#include "gx2r/resource.h"
+#include "gx2r/surface.h"
