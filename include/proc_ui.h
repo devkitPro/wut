501260e23576e7e4045374b0ba373abe38cd5624
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2026-08-01 23:46:57 +0200

    Add top level convinience headers for all RPL libraries

--- include/proc_ui.h
+++ include/proc_ui.h
@@ -1,6 +1,11 @@
+#pragma once
+
 /**
  * \defgroup proc_ui proc_ui
  *
  * Process management library used for managing transitions between foreground
  * and background applications.
  */
+
+#include <proc_ui/memory.h>
+#include <proc_ui/procui.h>
--- include/vpadbase.h
+++ include/vpadbase.h
@@ -1,5 +1,9 @@
+#pragma once
+
 /**
  * \defgroup vpadbase vpadbase
  *
  * Wii U Gamepad base library.
  */
+
+#include <vpadbase/base.h>
